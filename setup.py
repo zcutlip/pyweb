89278611ebcdcb478ca61c7ec8e772c8f11ba330
Zachary Cutlip <uid000@gmail.com>
2019-03-29 14:17:24 -0700

    add setup.py and create a pyweb module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='python-webserver',
+      version='0.2',
+      description='A basic localhost web server to use in your home directory.',
+      url="TBD",
+      packages=['pyweb'],
+      entry_points={
+          'console_scripts': ['pyweb=pyweb.command_line:main'],
+         },
+      python_requires='>=3.7',
+      )
